fc4a192a091185a769d453898cef809e72a77201
Bede Kelly <bedekelly97@gmail.com>
2015-02-22 23:58:58 +0000

    Remove unused imports

--- getmp3.py
+++ getmp3.py
@@ -1,9 +1,7 @@
 import subprocess
 import sys
-import os.path
-from pprint import pprint
 
 cmd = 'find /home/bede -name *.mp3'
-bytes = subprocess.check_output(cmd.split())
-text = bytes.decode(sys.stdout.encoding)
+bytes_ = subprocess.check_output(cmd.split())
+text = bytes_.decode(sys.stdout.encoding)
 mp3files = text.split("\n")
