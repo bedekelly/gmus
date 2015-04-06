e97aa18b9b872a625cfacf619a9e75d05afd8f44
Bede Kelly <bedekelly97@gmail.com>
2015-04-06 19:49:12 +0100

    Remove test file

--- getmp3.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-cmd = 'find /home/bede -name *.mp3'
-bytes_ = subprocess.check_output(cmd.split())
-text = bytes_.decode(sys.stdout.encoding)
-mp3files = text.split("\n")
